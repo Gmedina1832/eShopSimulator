6093310f29774c473f4f7b3b7300ef0724cc6fc8
GABRIEL MEDINA PEREIRA <GMEDINA1832@INTERBAYAMON.EDU>
2021-11-08 16:10:10 -0400

    classes UPDATES: eShopSimulator, Item;

--- Item.cs
+++ Item.cs
@@ -1,10 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace eShopSimulator
 {
     class Item
-    {
+    {        
+        public string name;
+        //public float price;
+        //public int stock;
+        public int itemCode;
+
+        public Item(string iName, int iCode)
+        {            
+            name = iName;
+            itemCode = iCode;
+            //price = iPrice;
+            //stock = iStock;                    
+        }
+        //List<Item> inventory = new List<Item>();
+        //List<Item> cart = new List<Item>();
+        //Item[] shirtInStock = new Item[20];
+        //Random random = new Random();
+
+        //Item shirt; 
+        //for (int i = 0; i < shirtInStock.Length; i++)
+        //{
+        //    shirt = new Item("Shirt", random.Next(GetHashCode()));
+        //    shirtInStock[i] = shirt;
+        //}
+
+        //foreach(Item i in shirtInStock)
+        //{
+        //    if (i.itemCode == shirtInStock[0].itemCode)
+        //    {
+        //        Console.WriteLine(i.itemCode);
+        //    }
+        //}
+        //Item pants = new Item("Pants", 10.99F, 20);
+        //inventory.Add(shirt);
+        //inventory.Add(pants);
+
+        //Console.WriteLine(inventory[0].name);
+
+        public override string ToString()
+        {
+            return $"{name,-10}" + $"{itemCode,-10}";
+            //return $"{name,-10}" + $"{price,-10}" + $"{stock,-10}";
+        }
+
     }
 }
